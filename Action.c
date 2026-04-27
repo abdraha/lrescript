51f0f046d60efd4df2f097cc51481d3b48c5e382
unknown <unknown>
2026-04-27 15:18:48 +0800

    commit6

---
+++ Action.c
@@ -0,0 +1,40 @@
+Action()
+{
+
+	web_url("Webtours", 
+		"URL=http://localhost:1080/Webtours", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_think_time(22);
+
+	lr_start_transaction("login");
+
+	web_submit_form("login.pl", 
+		"Snapshot=t2.inf", 
+		ITEMDATA, 
+		"Name=username", "Value=jojo", ENDITEM, 
+		"Name=password", "Value=bean", ENDITEM, 
+		"Name=login.x", "Value=48", ENDITEM, 
+		"Name=login.y", "Value=3", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("login",LR_AUTO);
+
+	lr_think_time(13);
+
+	lr_start_transaction("logout");
+
+	web_image("SignOff Button", 
+		"Alt=SignOff Button", 
+		"Snapshot=t3.inf", 
+		LAST);
+
+	lr_end_transaction("logout",LR_AUTO);
+
+	return 0;
+}
